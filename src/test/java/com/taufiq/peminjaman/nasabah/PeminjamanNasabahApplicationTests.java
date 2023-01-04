3218cd912fa494df11e87e040c149d9b67cf4d83
Muhammad Taufiqurrahman <113502696+TaufiqTzy@users.noreply.github.com>
2023-01-04 20:01:27 +0700

    Add files via upload

---
+++ src/test/java/com/taufiq/peminjaman/nasabah/PeminjamanNasabahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taufiq.peminjaman.nasabah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeminjamanNasabahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
